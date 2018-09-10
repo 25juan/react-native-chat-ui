ed4dc253e7d78a526b76c1b8e31779d0a7ad6870
孔东 <kongdong@repeatlink.cn>
2018-09-10 17:07:15 +0800

    example 初始化

--- example/App.js
+++ example/App.js
@@ -11,29 +11,11 @@ import {
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import ChatDemo from "./example" ;
 
 export default class App extends Component<{}> {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    return (<ChatDemo/>);
   }
 }
 
---
+++ example/mock.js
@@ -0,0 +1,48 @@
+var msg = {  // 文本
+    msgId: "msgid",
+    status: "send_failed",
+    isOutgoing: true,
+};
+var rightUser = {
+    _id: "",
+    name: "rightUser",
+    avatar: "http://app.yunsdt.com/app/images/qq.png"
+};
+var leftUser = {
+    _id: "",
+    name: "leftUser",
+    avatar: "http://app.yunsdt.com/app/images/avatar.png"
+}
+let counter = 0 ;
+export const mockText = (isOutgoing=true,text,status)=>{
+    const msgId = `msgid_${counter++}` ;
+    if(isOutgoing){
+        return { ...msg,text,isOutgoing,msgType: "text",status,fromUser:rightUser,msgId } ;
+    }
+    return { ...msg,text,isOutgoing,msgType: "text",status, msgId,fromUser:leftUser} ;
+};
+export const mockImage = (isOutgoing=true,url)=>{
+    const msgId = `msgid_${counter++}` ;
+    if(isOutgoing){
+        return { ...msg,msgId,fromUser:rightUser,isOutgoing,msgType:"image",status:"send_going",extend:{ imageHeight:80,imageWidth:50,thumbPath:url } } ;
+    }
+    return { ...msg,fromUser:leftUser,isOutgoing,msgId,msgType:"image",status:"send_success",extend:{ imageHeight:80,imageWidth:50,thumbPath:url } } ;
+};
+export const mockLocation = (isOutgoing=false)=>{
+    const msgId = `msgid_${counter++}` ;
+    if(isOutgoing){
+        return { ...msg,msgId,isOutgoing,fromUser:rightUser,title:"四川省成都市锦江区牛王庙",msgType: "location",status: "send_going", } ;
+    }
+    return { ...msg,isOutgoing,msgId,fromUser:leftUser,title:"四川省成都市锦江区牛王庙",msgType: "location",status: "send_going", } ;
+
+};
+export const mockVoice = (isOutgoing=false,isRead=false,playing=false)=>{
+    const msgId = `msgid_${counter++}` ;
+    if(isOutgoing){
+        return { ...msg,msgId,isOutgoing,fromUser:rightUser,playing,duration:1000,isRead,msgType: "voice",status: "send_going", } ;
+    }
+    return { ...msg,isOutgoing,msgId,fromUser:leftUser,playing,duration:1000,isRead,msgType: "voice",status: "send_going", } ;;
+
+};
+
+export default mockText ;
